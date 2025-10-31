460b1f73d25fbb1873adcadf6fbea6d81651f9e3
Carlos Omar Barajas Barragan <carlos.barajas@iteso.mx>
2025-10-30 21:22:19 -0600

    .

---
+++ Parcial 3/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef int boolean;
+#define TRUE 1
+#define FALSE 0
+
+typedef struct stack_str stack;
+
+stack * stack_create();
+
+void stack_push(stack * st, void * data);
+
+void * stack_pop(stack * st);
+
+void stack_destroy(stack * st);
+
+void * stack_top(stack * st);
+
+int stack_size();
+
+
+#endif
