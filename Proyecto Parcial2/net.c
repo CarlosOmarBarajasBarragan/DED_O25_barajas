2cde4a2d53d00b03bf898c85eae4fcef1750bcb1
carlavillagran4 <carla.villagran.469@gmail.com>
2025-11-06 21:56:00 -0600

    net_follow, net_print

--- Proyecto Parcial2/net.c
+++ Proyecto Parcial2/net.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "net.h"
 
 struct strUsuario { 
@@ -23,3 +24,56 @@ int  male_count;
 int  female_count; 
 }; 
 typedef struct str_net* net; 
+
+bool net_follow(net n,char username1[], char username2[]){
+    if(n==NULL) return;
+    if(strcmp(username1,username2)==0) return FALSE;    //strcmp retorna 0 si son iguales las cadenas
+    user u1= findUser(n, username1);
+    user u2=findUser(n,username2);
+    
+    if (u1==NULL || u2==NULL) return FALSE; 
+    if(u1->followCount==4) return FALSE;
+    
+    for(int i=0;i<u1->followCount;i++){
+        if(u1->follows[i]==u2) return FALSE;
+    }
+    u1->follows[u1->followCount]=u2;
+    u1->followCount++;
+    
+    return TRUE;
+}
+
+void net_print(net n,int part){
+    if (n==NULL)return;
+    user list;
+    if(part==BOYS|| part==GIRLS){
+        printf("List of %s sorted by age:\n",part==GIRLS? "girls":"boys");
+        list=(part==GIRLS)? n->list_female: n->list_male;
+
+        if(list==NULL) {
+            printf("No users\n");
+            return;
+        }
+        
+        while(list!=NULL){
+            printf("%s\n",list->username);
+            list=list->gender_next;
+        }
+        return;
+    }
+    list=n->list;
+    
+    if(list==NULL) {
+            printf("No users\n");
+            return;
+    }
+    while(list!=NULL){  //También se puede usar el contador de n->user_count
+        if(list->followCount){
+            printf("User %s follows %d user(s):\n",list->username,list->followCount);
+            for(int i=0;i<list->followCount;i++) 
+                printf("%d) %s\n",i+1,list->follows[i]->username);
+        }else printf("User %s does not follow anyone yet.\n",list->username);
+        
+        list=list->global_next;
+    }
+}
--- Proyecto Parcial2/net.h
+++ Proyecto Parcial2/net.h
@@ -1,6 +1,12 @@
 #ifndef NET_H
 #define NET_H
 
+typedef int bool;
+#define TRUE 1
+#define FALSE 0
 
+#define FULL 0
+#define BOYS 1
+#define GIRLS 2
 
 #endif 
