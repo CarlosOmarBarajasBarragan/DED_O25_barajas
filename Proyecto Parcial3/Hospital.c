2c8dad512ddc94aba114fcc3141cd0e784f32b22
Yeshua-Miranda <yeshua.miranda@iteso.mx>
2025-11-24 10:26:57 -0600

    .

--- Proyecto Parcial3/Hospital.c
+++ Proyecto Parcial3/Hospital.c
@@ -1,9 +1,27 @@
+#include<stdio.h>
 #include <stdlib.h>
 
+// .h
+#include "Hospital.h"
 
-struct paciente
+struct paciente_str
 {
     int id;
     char name[40];
     int urgencia;
+    // falta el stck
 };
+struct doctor_str
+{
+    int id;
+    char name[40];
+    // falta la cola
+};
+
+struct Hospital_Manager
+{
+    
+    
+};
+
+
---
+++ Proyecto Parcial3/Hospital.h
@@ -0,0 +1,7 @@
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
+
+typedef struct paciente_str paciente;
+typedef struct doctor_str doctor;
+
+#endif
