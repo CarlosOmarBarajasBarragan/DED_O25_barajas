5867e59eaede781fc579bfe874700883630467a0
Santiago104034 <victor.ferrer@iteso.mx>
2025-12-01 09:37:37 -0600

    si

--- Proyecto Parcial3/Hospital.c
+++ Proyecto Parcial3/Hospital.c
@@ -38,30 +38,30 @@ struct Hospital_Manager_str
 paciente * create_paciente(int id,char nombre[],int urgencia){
     paciente * nuevo_paciente = (paciente*) malloc(sizeof(paciente));
 
-    nuevo_paciente->id=id;
-    strcpy(nuevo_paciente->name,nombre);
-    nuevo_paciente->urgencia=urgencia;
+    nuevo_paciente->id = id;
+    strcpy(nuevo_paciente-> name, nombre);
+    nuevo_paciente->urgencia = urgencia;
 
-    nuevo_paciente->historial_medico=stack_create();
+    nuevo_paciente->historial_medico = stack_create();
     return nuevo_paciente;
 }
 
 
-doctor * create_doctor(int id,char name[]){
+doctor * create_doctor(int id, char name[]){
     doctor * nuevo_doctor = (doctor*) malloc(sizeof(doctor));
 
-    nuevo_doctor->id=id;
-    strcpy(nuevo_doctor->name,name);
+    nuevo_doctor->id = id;
+    strcpy(nuevo_doctor-> name, name);
 
-    nuevo_doctor->fila_pacientes=queue_create();
+    nuevo_doctor->fila_pacientes = queue_create();
     return nuevo_doctor;
 
 }
 
-hospital_manager * create_hospital_manager(int m,hash_func hash,CompareFunc compare){
+hospital_manager * create_hospital_manager(int m, hash_func hash, CompareFunc compare){
    hospital_manager * nuevo_hospital_manager = (hospital_manager*) malloc(sizeof(hospital_manager));
 
-   nuevo_hospital_manager->lista_doctores=map_create(m,hash,compare);
+   nuevo_hospital_manager->lista_doctores = map_create(m, hash, compare);
    // Falta el grafo y el priority queue
 }
 
